c3778ad54e10b0cac85729caf08447b5881a0849
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-09-20 10:35:37 -0400

    - basic functionality working: scan start + stop, discover, connect + disconnect

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 var CentralManager = require('./lib/central-manager');
-var CentralManagerDelegate = require('./lib/central-manager-delegate');
+var centralManager = new CentralManager();
 
-var centralManagerDelegate = new CentralManagerDelegate();
-var centralManager = new CentralManager(centralManagerDelegate);
+centralManager.on('stateUpdate', function(state) {
+  console.log('stateUpdate => ', state);
 
-centralManagerDelegate.on('stateUpdate', function(state) {
-  console.log(state);
-
+  console.log('scanForPeripherals');
   centralManager.scanForPeripherals();
+});
+
+centralManager.on('peripheralDiscover', function(peripheral, advertisementData, rssi) {
+  console.log('peripheralDiscover => ', peripheral.identifier, advertisementData, rssi);
+
+  if (advertisementData.localName == 'CC2650 SensorTag') {
+    console.log('stopScan');
+    centralManager.stopScan();
+
+    console.log('connect');
+    centralManager.connect(peripheral);
+  }
+});
+
+centralManager.on('peripheralConnect', function(peripheral) {
+  console.log('peripheralConnect =>', peripheral.identifier);
+
+  peripheral.once('rssiUpdate', function(rssi, error) {
+    console.log('peripheral rssiUpdate =>', rssi, error);
+
+    console.log('cancelConnection');
+    centralManager.cancelConnection(peripheral);
+  });
+
+  peripheral.readRSSI();
+});
+
+centralManager.on('peripheralDisconnect', function(peripheral, error) {
+  console.log('peripheralDisconnect =>', peripheral.identifier, error);
+
+  process.exit(0);
+});
+
+centralManager.on('peripheralConnectFail', function(peripheral, error) {
+  console.log('peripheralConnectFail =>', peripheral.identifier, error);
+
+  process.exit(0);
 });
