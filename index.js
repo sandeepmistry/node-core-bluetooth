8f0edcd74d149a06098797475343b7b4fca29aa1
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-09-26 10:19:39 -0400

    - characteristic: read, write, notify, broadcast + descriptor: read, write

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ centralManager.on('peripheralDiscover', function(peripheral, advertisementData, 
         service.discoverCharacteristics();
       });
 
-
       service.on('characteristicsDiscover', function(characteristics, error) {
         console.log('\tservice characteristicsDiscover =>', characteristics.toString(), error);
 
@@ -70,8 +69,58 @@ centralManager.on('peripheralDiscover', function(peripheral, advertisementData, 
         characteristic.on('descriptorsDiscover', function(descriptors, error) {
           console.log('\tcharacteristic descriptorsDiscover =>', descriptors.toString(), error);
 
+          if (descriptors && descriptors.length) {
+            var descriptor = descriptors[0];
+
+            console.log('descriptor - readValue');
+            descriptor.readValue();
+
+            descriptor.on('valueUpdate', function(value, error) {
+              console.log('\tdescriptor valueUpdate =>', value.toString('hex'), error);
+            });
+
+            // console.log('descriptor - writeValue');
+            // descriptor.writeValue(new Buffer('0000', 'hex'));
+
+            // descriptor.on('valueWrite', function(error) {
+            //   console.log('\tdescriptor valueWrite =>', error);
+            // });
+          }
+
+          if (characteristic.properties.indexOf('read') !== -1) {
+            console.log('characteristic - readValue');
+            characteristic.readValue();
+          } else if (characteristic.properties.indexOf('write') !== -1) {
+            console.log('characteristic - writeValue');
+            characteristic.writeValue(new Buffer('hello'));
+          } else if (characteristic.properties.indexOf('writeWithoutResponse') !== -1) {
+            console.log('characteristic - writeValue');
+            characteristic.writeValue(new Buffer('hello'), true);
+          } else if (characteristic.properties.indexOf('broadcast') !== -1) {
+            console.log('characteristic - setBroadcastValue');
+            characteristic.setBroadcastValue(true);
+          } else if (characteristic.properties.indexOf('notify') !== -1 || characteristic.properties.indexOf('indicate') !== -1) {
+            console.log('characteristic - setNotifyValue');
+            characteristic.setNotifyValue(true);
+          }
+        });
+
+        characteristic.on('valueUpdate', function(value, error) {
+          console.log('\tcharacteristic valueUpdate =>', value.toString('hex'), error);
+
           console.log('cancelConnection');
           peripheral.cancelConnection();
+        });
+
+        characteristic.on('valueWrite', function(error) {
+          console.log('\tcharacteristic valueWrite =>', error);
+
+          console.log('cancelConnection');
+          peripheral.cancelConnection();
+        });
+
+        characteristic.on('notificationStateUpdate', function(state, error) {
+          console.log('\tcharacteristic notificationStateUpdate =>', state, error);
         });
       });
     });
